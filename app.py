3234beefc96715ba56e3fc452ac5e5c98815a23e
Krishnan Authi Narayanan <Krishnan.AuthiNaraynan@softwareag.com>
2023-09-25 17:13:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -125,17 +125,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-df = pd.DataFrame(
-    [
-        {"command": "st.selectbox", "rating": 4, "is_widget": True},
-        {"command": "st.balloons", "rating": 5, "is_widget": False},
-        {"command": "st.time_input", "rating": 3, "is_widget": True},
-    ]
-)
-
-st.dataframe(df, use_container_width=True)
